f01460c59a4d34b724734c36d55f51c7d88a152c
Thuonglepham2k2 <2051063468@e.tlu.edu.vn>
2023-05-20 07:04:38 +0700

    Form

--- BTTH01/From.php
+++ BTTH01/From.php
@@ -25,3 +25,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Add New Student</title>
+</head>
+<body>
+  <h1>Add New Student</h1>
+  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    ID: <input type="text" name="id"><br>
+    Name: <input type="text" name="name"><br>
+    Age: <input type="text" name="age"><br>
+    Grade: <input type="text" name="grade"><br>
+    <input type="submit" name="submit" value="Add Student">
+  </form>
+</body>
+</html>
