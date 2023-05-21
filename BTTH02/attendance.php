db96c52ffbd91949dbaf11ca70c66ed3f9d3d34a
Thuonglepham2k2 <2051063468@e.tlu.edu.vn>
2023-05-21 22:13:09 +0700

    add attendance.php

---
+++ BTTH02/attendance.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Điểm danh sinh viên</title>
+</head>
+<body>
+
+    <h1>Điểm danh sinh viên</h1>
+    <form method="POST">
+        <label for="maSinhVien">Mã sinh viên:</label>
+        <input type="text" id="maSinhVien" name="maSinhVien" required>
+        
+        <label for="maLop">Mã lớp:</label>
+        <input type="text" id="maLop" name="maLop" required>
+        
+        <input type="submit" value="Điểm danh">
+    </form>
+</body>
+</html>
